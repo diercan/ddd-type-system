2a05c0e10b7a12d5ddd5fa7d4757de187865f5f2
Daniel Iercan <daniel.iercan@theaccessgroup.com>
2023-02-13 12:31:34 +0200

    use specific types for create invoice workflow

--- src/Sample.FinanceSystem/Sample.FinanceSystem.Domain/Workflows/CreateInvoiceWorkflow.cs
+++ src/Sample.FinanceSystem/Sample.FinanceSystem.Domain/Workflows/CreateInvoiceWorkflow.cs
@@ -9,27 +9,27 @@ using static Sample.FinanceSystem.Domain.Types.InvoiceEntity;
 namespace Sample.FinanceSystem.Domain.Workflows;
 public class CreateInvoiceWorkflow : Workflow<IInvoiceEntity, InvoiceDto, InvoiceContext, IInvoiceCreatedEvent>
 {
-    protected override Task<IInvoiceCreatedEvent> ConvertResultToEvent<TValidEntity>(EitherAsync<IErrorMessage, TValidEntity> result)
+    protected override Task<IInvoiceCreatedEvent> ConvertResultToEvent<CalculatedInvoice>(EitherAsync<IErrorMessage, CalculatedInvoice> result)
     {
         throw new NotImplementedException();
     }
 
-    protected override Either<IErrorMessage, TUnvalidatedEntity> MapDtoToEntity<TUnvalidatedEntity>(InvoiceDto input)
+    protected override Either<IErrorMessage, UnvalidatedInvoice> MapDtoToEntity<UnvalidatedInvoice>(InvoiceDto input)
     {
         throw new NotImplementedException();
     }
 
-    protected override Either<IErrorMessage, TValidEntity> RunBusinessRules<TValidEntity, TUnvalidatedEntity>(TUnvalidatedEntity inputEntity, InvoiceContext context)
+    protected override Either<IErrorMessage, CalculatedInvoice> RunBusinessRules<CalculatedInvoice, UnvalidatedInvoice>(UnvalidatedInvoice inputEntity, InvoiceContext context)
     {
         throw new NotImplementedException();
     }
 
-    protected override TryAsync<InvoiceContext> TryLoadDbContext<TUnvalidatedEntity>(TUnvalidatedEntity entity)
+    protected override TryAsync<InvoiceContext> TryLoadDbContext<UnvalidatedInvoice>(UnvalidatedInvoice entity)
     {
         throw new NotImplementedException();
     }
 
-    protected override TryAsync<Unit> TrySaveToDb<TValidEntity>(TValidEntity entity)
+    protected override TryAsync<Unit> TrySaveToDb<CalculatedInvoice>(CalculatedInvoice entity)
     {
         throw new NotImplementedException();
     }
