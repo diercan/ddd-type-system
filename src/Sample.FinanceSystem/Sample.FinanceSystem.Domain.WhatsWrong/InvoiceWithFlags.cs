3fe39da46d35004ad9b28e11dc58661df11bb62e
Daniel Iercan <daniel.iercan@theaccessgroup.com>
2023-03-12 10:01:38 +0200

    Added falgs example

---
+++ src/Sample.FinanceSystem/Sample.FinanceSystem.Domain.WhatsWrong/InvoiceWithFlags.cs
@@ -0,0 +1,17 @@
+﻿namespace Sample.FinanceSystem.Domain.WhatsWrong;
+
+public record InvoiceWithFlags
+{
+    //...
+
+    public bool IsValid { get; set; }
+    public decimal Total { get; set; }
+
+    public bool IsApproved { get; set; }
+    public string ApproverName { get; set; }
+
+    public bool IsPaid { get; set; }
+    public PaymentDetails PaymentDetails { get; set; }
+
+    //...
+}
